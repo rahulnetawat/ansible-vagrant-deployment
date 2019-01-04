1e8ad08929c8aacc388e04adcce789fea60c2b67
Rahul Netawat <rahul.netawat@webdunia.net>
2019-01-04 21:28:28 +0530

    vagrant installation script.

--- install-vagrant.sh
+++ install-vagrant.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# This script is only for ubuntu.
 wget https://releases.hashicorp.com/vagrant/2.2.2/vagrant_2.2.2_x86_64.deb
 dpkg -i vagrant_2.2.2_x86_64.deb
+
+# Verify Vagrant Version by below command.
+vagrant version
